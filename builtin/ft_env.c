fd5f502e5b5bcd19c08a9ca226a928ad123ee098
Luiz henrique Liberal de souza <lliberal@c2r8s1.42lisboa.com>
2023-05-17 22:06:23 +0100

    17-May

--- builtin/ft_env.c
+++ builtin/ft_env.c
@@ -6,11 +6,46 @@
 /*   By: lliberal <lliberal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 12:43:37 by lliberal          #+#    #+#             */
-/*   Updated: 2023/05/10 19:28:52 by lliberal         ###   ########.fr       */
+/*   Updated: 2023/05/17 22:05:57 by lliberal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+void	print_2d(char **env)
+{
+	int	i;
+
+	i = -1;
+	while (env[++i])
+		printf("%s\n", env[i]);
+}
+
+int	ft_strlen_2d(char **a)
+{
+	int	i;
+
+	i = 0;
+	if (!a)
+		return (0);
+	while (a[i])
+		i++;
+	return (i);
+}
+
+char	**synchronize_env(char **env, char *cmd)
+{
+	char	**new_env;
+	int		i;
+
+	i = -1;
+	new_env = increase_env(env);
+	while (new_env[++i])
+		new_env[i] = new_env[i];
+	new_env[i] = ft_strdup(cmd);
+	free(env);
+	return (new_env);
+}
 
 int	execute_env(t_cmd *cmd)
 {
